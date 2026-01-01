2681dc19d002e5de8683d29fc402c55a93a14c52
sky-monarch <cca06@mail.ru>
2026-01-02 00:03:37 +0300

    checking for work postgreeSQL

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
 <?php
-echo "Hello world!";
+$pdo = new PDO('pgsql:host=localhost;dbname=shop', 'sky','Marmakmr2.0');
+
+$query = 'SELECT VERSION() AS version';
+$ver = $pdo->query($query);
+$version = $ver->fetch();
+echo $version['version'];
